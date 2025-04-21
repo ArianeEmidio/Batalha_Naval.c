81fcc1cf41d83f155f98a516c7b4712ae3bd499b
Ariane Emidio <ariane.emidio1991@gmail.com>
2025-04-21 11:36:02 -0300

    Update Batalha_Naval.c
    Adicionando novas funcionalidades para o desafio nivel 3 - Mestre!!

--- Batalha_Naval.c
+++ Batalha_Naval.c
@@ -1,61 +1,104 @@
 #include <stdio.h>
 
-#define TAMANHO_TABULEIRO 10
-#define TAMANHO_NAVIO      3
-#define AGUA               0
-#define NAVIO              3
+#define TAMANHO_TABULEIRO   10
+#define TAMANHO_NAVIO        3
+#define AGUA                 0
+#define NAVIO                3
+#define HABILIDADE           5
+
+// Função para aplicar uma matriz de habilidade ao tabuleiro
+void aplicarHabilidade(int tabuleiro[][TAMANHO_TABULEIRO], int habilidade[5][5], int origemLinha, int origemColuna) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (habilidade[i][j] == 1) {
+                int linhaTabuleiro = origemLinha - 2 + i;
+                int colunaTabuleiro = origemColuna - 2 + j;
+                if (linhaTabuleiro >= 0 && linhaTabuleiro < TAMANHO_TABULEIRO &&
+                    colunaTabuleiro >= 0 && colunaTabuleiro < TAMANHO_TABULEIRO &&
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] == AGUA) {
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] = HABILIDADE;
+                }
+            }
+        }
+    }
+}
 
 int main() {
-    char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
 
-    // 1) Inicializa o tabuleiro com água
-    for (int i = 0; i < TAMANHO_TABULEIRO; i++)
-        for (int j = 0; j < TAMANHO_TABULEIRO; j++)
+    // Inicializa o tabuleiro com água
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
             tabuleiro[i][j] = AGUA;
-
-    // 2) Posiciona navio horizontal em C–E na linha 3
-    int linha = 3, coluna = 2;
-    if (coluna + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
-        for (int i = 0; i < TAMANHO_NAVIO; i++)
-            tabuleiro[linha][coluna + i] = NAVIO;
+        }
     }
 
-    // 3) Posiciona navio vertical em H nas linhas 5–7
-    linha = 5; coluna = 7;
-    if (linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
-        for (int i = 0; i < TAMANHO_NAVIO; i++)
-            tabuleiro[linha + i][coluna] = NAVIO;
-    }
+    // Posicionar navios:
+    tabuleiro[3][1] = NAVIO;
+    tabuleiro[3][2] = NAVIO;
+    tabuleiro[3][3] = NAVIO;
 
-    // 4) Posiciona navio diagonal principal (descendo para a direita) F0, G1, H2
-    linha = 0; coluna = 5;
-    if (linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO && coluna + TAMANHO_NAVIO <= TAMANHO_TABULEIRO) {
-        for (int i = 0; i < TAMANHO_NAVIO; i++)
-            tabuleiro[linha + i][coluna + i] = NAVIO;
-    }
+    tabuleiro[5][9] = NAVIO;
+    tabuleiro[6][9] = NAVIO;
+    tabuleiro[7][9] = NAVIO;
 
-    // 5) Posiciona navio diagonal secundária (descendo para a esquerda) F7, E8, D9
-    linha = 7; coluna = 5;
-    if (linha + TAMANHO_NAVIO <= TAMANHO_TABULEIRO && coluna - (TAMANHO_NAVIO - 1) >= 0) {
-        for (int i = 0; i < TAMANHO_NAVIO; i++)
-            tabuleiro[linha + i][coluna - i] = NAVIO;
-    }
+    tabuleiro[0][4] = NAVIO;
+    tabuleiro[1][5] = NAVIO;
+    tabuleiro[2][6] = NAVIO;
 
-    // 6) Imprime o título
+    tabuleiro[7][7] = NAVIO;
+    tabuleiro[8][6] = NAVIO;
+    tabuleiro[9][5] = NAVIO;
+
+    // MATRIZ: Habilidade Cone
+    int cone[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0}
+    };
+
+    // MATRIZ: Habilidade Cruz
+    int cruz[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0},
+        {1, 1, 1, 1, 1},
+        {0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    // MATRIZ: Habilidade Octaedro (losango)
+    int octaedro[5][5] = {
+        {0, 0, 1, 0, 0},
+        {0, 1, 1, 1, 0},
+        {1, 1, 1, 1, 1},
+        {0, 1, 1, 1, 0},
+        {0, 0, 1, 0, 0}
+    };
+
+    // Aplicar as habilidades ao tabuleiro:
+    aplicarHabilidade(tabuleiro, cone, 2, 2);        // Cone no canto superior esquerdo
+    aplicarHabilidade(tabuleiro, cruz, 3, 7);        // Cruz no canto superior direito
+    aplicarHabilidade(tabuleiro, octaedro, 7, 2);    // Octaedro no canto inferior esquerdo
+
+    // Exibir nome do jogo
     printf("\n-- TABULEIRO BATALHA NAVAL --\n\n");
 
-    // 7) Cabeçalho de colunas
+    // Exibe o cabeçalho das colunas
     printf("   ");
-    for (int j = 0; j < TAMANHO_TABULEIRO; j++)
+    for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
         printf("%c ", colunas[j]);
+    }
     printf("\n");
 
-    // 8) Exibe cada linha do tabuleiro
+    // Exibe o tabuleiro
     for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
         printf("%2d ", i);
-        for (int j = 0; j < TAMANHO_TABULEIRO; j++)
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
             printf("%d ", tabuleiro[i][j]);
+        }
         printf("\n");
     }
 
